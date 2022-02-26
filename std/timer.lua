e0712bfed743f19156bc99be381053b867f81fae
RiskoZoSlovenska <riskozoslovenska+git@gmail.com>
2022-02-26 18:27:45 -0500

    Use alternate `timer` function names

--- std/timer.lua
+++ std/timer.lua
@@ -14,23 +14,21 @@ function timer.sleep(delay, thread) end
 ---@param ... any
 ---@return uv_timer_t
 ---@error nil, string, string
-function timer.setTimeout(delay, callback, ...) end
+function timer.delay(delay, callback, ...) end
 
 ---@param delay number
 ---@param callback function
 ---@param ... any
 ---@return uv_timer_t
 ---@error nil, string, string
-function timer.setInterval(delay, callback, ...) end
-
----@param timer_t uv_timer_t
-function timer.clearInterval(timer_t) end
-
-timer.clearTimeout = timer.clearInterval
+function timer.periodically(delay, callback, ...) end
 
 ---@param callback function
 ---@param ... any
 ---@error nil, string, string
-function timer.setImmediate(callback, ...) end
+function timer.immediately(callback, ...) end
+
+---@param timer_t uv_timer_t
+function timer.clear(timer_t) end
 
 return timer

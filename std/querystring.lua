d8f4f6372ed30b76e1f28316e52abb860f4de27f
RiskoZoSlovenska <riskozoslovenska+git@gmail.com>
2022-02-26 17:42:44 -0500

    Don't shadow `table` global

--- std/querystring.lua
+++ std/querystring.lua
@@ -1,11 +1,11 @@
 ---@class std.querystring
 local querystring = {}
 
----@param table table
+---@param tbl table
 ---@param separator_char? string
 ---@param equals_char? string
 ---@return string
-function querystring.encode(table, separator_char, equals_char) end
+function querystring.encode(tbl, separator_char, equals_char) end
 
 ---@param str string
 ---@param separator_char? string

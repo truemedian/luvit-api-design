370d05e1a68b2c98b8cd106d8c3f4747b75e5c05
Bilal2453 <belal2453@gmail.com>
2022-06-14 22:24:37 +0300

    codec: add websocket field

--- std/codec.lua
+++ std/codec.lua
@@ -11,6 +11,7 @@ local codec = {}
 ---@alias std.codec.encoder function function(any) -> string
 
 codec.http = require('std.codec.http')
+codec.websocket = require('std.codec.websocket')
 
 ---@param read std.codec.reader
 ---@param decode std.codec.decoder

d90f6191d7718e540d4d3e6f49599c6807bc26bd
RiskoZoSlovenska <riskozoslovenska+git@gmail.com>
2023-10-05 12:07:18 -0400

    Improve timer docs and add `timer.tick()`

--- deps/timer.lua
+++ deps/timer.lua
@@ -6,9 +6,8 @@ local timer = {}
 
 ---@alias uv_timer_t userdata
 
+---Yield the current coroutine for at least `delay` milliseconds.
 ---@param delay number
----@return uv_timer_t
----@error nil, string, string
 function timer.sleep(delay)
     local thread = coroutine.running()
     local timer_obj = uv.new_timer()
@@ -20,11 +19,18 @@ function timer.sleep(delay)
     return coroutine.yield()
 end
 
+---Yield the current coroutine until the next event loop tick.
+function timer.tick()
+    local thread = coroutine.running()
+    timer.immediately(utils.assertResume, thread)
+    return coroutine.yield()
+end
+
+---Call `callback` after at least `delay` milliseconds.
 ---@param delay number
 ---@param callback fun(...: any)
----@param ... any
+---@param ... any Arguments to pass to `callback`
 ---@return uv_timer_t
----@error nil, string, string
 function timer.delay(delay, callback, ...)
     local timer_obj = uv.new_timer()
     local args = { ... }
@@ -37,11 +43,11 @@ function timer.delay(delay, callback, ...)
     return timer_obj
 end
 
+---Call `callback` every at least `delay` milliseconds.
 ---@param delay number
 ---@param callback fun(...: any)
----@param ... any
+---@param ... any Arguments to pass to `callback`
 ---@return uv_timer_t
----@error nil, string, string
 function timer.periodically(delay, callback, ...)
     local timer_obj = uv.new_timer()
     local args = { ... }
@@ -52,11 +58,12 @@ function timer.periodically(delay, callback, ...)
     return timer_obj
 end
 
+---Call `callback` on the next event loop tick.
 ---@param callback fun(...: any)
----@param ... any
----@error nil, string, string
+---@param ... any Arguments to pass to `callback`
 function timer.immediately(callback, ...) end
 
+---Stop and close a running timer handle.
 ---@param timer_obj uv_timer_t
 function timer.clear(timer_obj)
     if uv.is_closing(timer_obj) then return end
